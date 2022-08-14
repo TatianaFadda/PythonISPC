8476300691d00eb8b7e2cf2b290004ca0f1fc651
TatianaFadda <tatianafadda93@gmail.com>
2022-08-13 22:36:33 -0300

    first commit

---
+++ Ejercicio9.py
@@ -0,0 +1,9 @@
+# Ejercicio 9 Tupla "Historial5" representa montos de atención en pesos de diferentes servicios/consultas de la mascota "Toto"
+
+historial5 = (8520, 9510, 7530, 3570, 1590)
+lista = list(historial5)
+
+valorMaxConsulta = max(lista)
+
+print("El valor máximo gastado en las consultas de Toto fue:", valorMaxConsulta)
+
